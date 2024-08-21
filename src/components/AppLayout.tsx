106a4f0d41d1e2d6a40727392e0ce4688d43c9db
Jason McCandless <jhmccandless1105@gmail.com>
2024-08-20 22:15:49 -0500

    added a refresh handling

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,11 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import AppMenu from "./AppMenu";
 import FormResetAlert from "./FormResetAlert";
+import { useAppSelector } from "./hooks";
 
 function AppLayout(props: any) {
+  // const dispatch = useAppDispatch();
+
+  const formStateValues = useAppSelector((state: any) => state.formState);
+
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  useEffect(() => {
+    if (Object.keys(formStateValues.dirtyFields).length !== 0) {
+      const handleBeforeUnload = (e: any) => {
+        e.preventDefault();
+
+        // dispatch(openAlert("/"));
+      };
+      window.addEventListener("beforeunload", handleBeforeUnload);
+      return () => {
+        window.removeEventListener("beforeunload", handleBeforeUnload);
+      };
+    }
+  });
 
   function onPageClick() {
     if (isMenuOpen) setIsMenuOpen(false);
