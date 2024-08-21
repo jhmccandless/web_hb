b06b4da23cde20bb8b57f77efb9f7a102194eb7e
Jason McCandless <jhmccandless1105@gmail.com>
2024-08-20 19:20:11 -0500

    have alerts working for both manu and back button

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -3,17 +3,17 @@ import Header from "./Header";
 import Footer from "./Footer";
 import AppMenu from "./AppMenu";
 import FormResetAlert from "./FormResetAlert";
-import { useAppSelector } from "./hooks";
+// import { useAppSelector } from "./hooks";
 
 function AppLayout(props: any) {
-  const formStateValues = useAppSelector((state: any) => state.formState);
+  // const formStateValues = useAppSelector((state: any) => state.formState);
 
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [isAlertOpen, setIsAlertOpen] = useState(false);
-  const [canAlertOpen, setCanAlertOpen] = useState(
-    Object.keys(formStateValues.dirtyFields).length !== 0
-  );
-  const [formAlertURL, setFormAlertURL] = useState("/");
+  // const [isAlertOpen, setIsAlertOpen] = useState(false);
+  // const [canAlertOpen, setCanAlertOpen] = useState(
+  //   Object.keys(formStateValues.dirtyFields).length !== 0
+  // );
+  // const [formAlertURL, setFormAlertURL] = useState("/");
 
   function onPageClick() {
     if (isMenuOpen) setIsMenuOpen(false);
--- src/components/MenuCard.tsx
+++ src/components/MenuCard.tsx
@@ -1,11 +1,22 @@
 import { useNavigate } from "react-router-dom";
+import { useAppSelector } from "./hooks";
+import { useDispatch } from "react-redux";
+import { openAlert } from "../appSlices/formSlice";
 
 function MenuCard(props: any) {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const formStateValues = useAppSelector((state: any) => state.formState);
 
   function onCardClick() {
-    navigate(props.desNav);
-    props.toggleButton(false);
+    if (Object.keys(formStateValues.dirtyFields).length === 0) {
+      navigate(props.desNav);
+      props.toggleButton(false);
+    } else {
+      dispatch(openAlert(props.desNav));
+    }
+    // navigate(props.desNav);
   }
 
   return (
