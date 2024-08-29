2f758c32b6b729a1c4c3406a493225e461b1d7c5
Jason McCandless <jhmccandless1105@gmail.com>
2024-08-29 00:07:50 -0500

    workout for repeaters works in landscape

--- src/components/RepeaterTimerDetails.tsx
+++ src/components/RepeaterTimerDetails.tsx
@@ -5,42 +5,42 @@ function RepeaterTimerDetails(props: any) {
   // console.log(props);
   return (
     <div className="timer-wrapper-details">
-      <div className="timer-details-top">
-        <IncrementTime
-          action={"hang"}
-          currentAct={props.currentAct}
-          actionTime={props.actionTime}
-          timerState={props.timerState.hangTime}
-          stylingProp="timer-item-1-1"
-        />
+      {/* <div className="timer-details-top"> */}
+      <IncrementTime
+        action={"hang"}
+        currentAct={props.currentAct}
+        actionTime={props.actionTime}
+        timerState={props.timerState.hangTime}
+        stylingProp="timer-item-1-1 timer-item-1-1-land"
+      />
 
-        <IncrementTime
-          action={"off"}
-          currentAct={props.currentAct}
-          actionTime={props.actionTime}
-          timerState={props.timerState.offTime}
-          stylingProp="timer-item-1-2"
-        />
-        <IncrementTime
-          action={"rest"}
-          currentAct={props.currentAct}
-          actionTime={props.actionTime}
-          timerState={props.timerState.restTime}
-          stylingProp="timer-item-1-3"
-        />
-      </div>
-      <div className="timer-details-bottom">
-        <IncrementCounter
-          whichCounter={"reps"}
-          stylingProp="timer-item-2-1 timer-item-left"
-          counter={props.repsCounter}
-        />
-        <IncrementCounter
-          whichCounter={"sets"}
-          counter={props.setsCounter}
-          stylingProp="timer-item-2-3 timer-item-right"
-        />
-      </div>
+      <IncrementTime
+        action={"off"}
+        currentAct={props.currentAct}
+        actionTime={props.actionTime}
+        timerState={props.timerState.offTime}
+        stylingProp="timer-item-1-2 timer-item-1-3-land"
+      />
+      <IncrementTime
+        action={"rest"}
+        currentAct={props.currentAct}
+        actionTime={props.actionTime}
+        timerState={props.timerState.restTime}
+        stylingProp="timer-item-1-3 timer-item-1-2-land"
+      />
+      {/* </div> */}
+      {/* <div className="timer-details-bottom timer-item-2-2-land"> */}
+      <IncrementCounter
+        whichCounter={"reps"}
+        stylingProp="timer-item-2-1 timer-item-left timer-item-2-2-land"
+        counter={props.repsCounter}
+      />
+      <IncrementCounter
+        whichCounter={"sets"}
+        counter={props.setsCounter}
+        stylingProp="timer-item-2-3 timer-item-right timer-item-3-3-land"
+      />
+      {/* </div> */}
     </div>
   );
 }
