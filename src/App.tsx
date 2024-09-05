38f002a0f395a0412a4ff16d275d162dbc1b3a2b
Jason McCandless <67750293+jhmccandless@users.noreply.github.com>
2024-09-04 21:27:51 -0500

    Web_branch_3 (#15)

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,37 @@
 import "./App.css";
-import TimerForm from "./components/TimerForm";
+import RepeaterTimerForm from "./components/RepeaterTimerForm";
 import { Provider } from "react-redux";
 import { store } from "./appSlices/store";
 import AppLayout from "./components/AppLayout";
 import { Route, Routes } from "react-router-dom";
-import Timer3UI from "./components/RepeaterTimer";
+import RepeaterTimer from "./components/RepeaterTimer";
 import NonRoute from "./components/NonRoute";
 import AboutPage from "./components/AboutPage";
 import HomePage from "./components/HomePage";
+import OnOffTimerForm from "./components/OnOffTimerForm";
+import OnOffTimer from "./components/OnOffTimer";
 
 function App() {
   return (
     <Provider store={store}>
       <Routes>
         <Route path="/" element={<AppLayout desComp={<HomePage />} />} />
-        <Route path="/form" element={<AppLayout desComp={<TimerForm />} />} />
-        <Route path="/workout" element={<AppLayout desComp={<Timer3UI />} />} />
+        <Route
+          path="/form-on-off"
+          element={<AppLayout desComp={<OnOffTimerForm />} />}
+        />
+        <Route
+          path="/form-repeaters"
+          element={<AppLayout desComp={<RepeaterTimerForm />} />}
+        />
+        <Route
+          path="/workout-on-off"
+          element={<AppLayout desComp={<OnOffTimer />} />}
+        />
+        <Route
+          path="/workout-repeaters"
+          element={<AppLayout desComp={<RepeaterTimer />} />}
+        />
         <Route path="/about" element={<AppLayout desComp={<AboutPage />} />} />
         <Route path="*" element={<NonRoute />} />
       </Routes>
---
+++ src/components/OnOffTimerForm.tsx
@@ -0,0 +1,115 @@
+import { useEffect, useState } from "react";
+import TimeInputForForm from "./TimeInputForForm";
+import { setWorkoutValues } from "../appSlices/timerSlice";
+import { useAppDispatch, useAppSelector } from "../hooks/hooks";
+import { useNavigate } from "react-router-dom";
+import { openAlert } from "../appSlices/formSlice";
+import CounterInputForForm from "./CounterInputForForm";
+
+export function checkValues(refObj: any, newObj: any) {
+  const finalObj: any = {};
+  Object.entries(refObj).forEach(([key, val]) => {
+    finalObj[key] = newObj[key] ? newObj[key] : val;
+  });
+
+  return finalObj;
+}
+
+function OnOffTimerForm() {
+  const timerValues = useAppSelector((state: any) => state.timerInfo);
+  const formStateValues = useAppSelector((state: any) => state.formState);
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const [timeObject, setTimeObject] = useState<any>({
+    // hangTime: "",
+    // offTime: "",
+    // restTime: "",
+    // repCount: "",
+    // setCount: "",
+    // delayStartTime: "",
+  });
+
+  useEffect(() => {
+    if (!timerValues.timerType) {
+      navigate("/");
+    }
+  });
+
+  function handleSubmit(e: any) {
+    e.preventDefault();
+    dispatch(setWorkoutValues(checkValues(timerValues.timerTimes, timeObject)));
+    navigate("/workout-on-off");
+  }
+
+  function onBackClick() {
+    if (Object.keys(formStateValues.dirtyFields).length === 0) navigate("/");
+    else {
+      dispatch(openAlert("/"));
+    }
+  }
+
+  return (
+    <form className="form-onoff" onSubmit={handleSubmit}>
+      <h2
+        className="form-row-title-onoff"
+        style={{ display: "flex", alignItems: "center", margin: "0" }}
+      >
+        Fill In Your Workout
+      </h2>
+      {/* <div className="form-row">
+        <label>
+          <select style={{ width: "100px" }}>
+            <option value="none" style={{ display: "none" }}>
+              Workout Type Select
+            </option>
+            <option value="repeaters">Repeaters</option>
+            <option value="on-off">On-Off</option>
+            <option value="circuit">Circuit</option>
+          </select>
+        </label>
+      </div> */}
+      {Object.entries(timerValues.timerTimes)
+        .filter(([key, val]) => val !== -1)
+        .map(([key, val], i: number) => {
+          if (key.includes("Time")) {
+            return (
+              <div key={i} className={"form-row"}>
+                <TimeInputForForm
+                  placeHolderData={timerValues.timerTimes}
+                  timeObject={timeObject}
+                  whichTimeInput={key}
+                  setTimeObject={setTimeObject}
+                />
+              </div>
+            );
+          } else {
+            return (
+              <div key={i} className={"form-row"}>
+                <CounterInputForForm
+                  placeHolderData={timerValues.timerTimes}
+                  timeObject={timeObject}
+                  whichTimeInput={key}
+                  setTimeObject={setTimeObject}
+                />
+              </div>
+            );
+          }
+        })}
+      <div className="form-row-buttons-onoff">
+        <div className="workout-button-div-onoff">
+          <button type="submit" className="form-button">
+            Workout!
+          </button>
+        </div>
+        <div className="back-button-div-onoff">
+          <button type="button" className="form-button" onClick={onBackClick}>
+            Back
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+}
+
+export default OnOffTimerForm;
