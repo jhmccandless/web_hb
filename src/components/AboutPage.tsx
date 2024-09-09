9fc0f515871a470ae65dc4dba43f229140714aa3
Jason McCandless <jhmccandless1105@gmail.com>
2024-09-08 23:00:33 -0500

    added an about page

--- src/components/AboutPage.tsx
+++ src/components/AboutPage.tsx
@@ -1,7 +1,29 @@
 function AboutPage() {
   return (
-    <div>
-      <p>About this app</p>
+    <div className="about-page">
+      <h3>Aboout the Hangboarding Application</h3>
+      <div className="about-page-details">
+        <p>
+          Say goodbye to scrambling to chalk up before you start your first hang
+          and to getting your phone chalked up as well. And Say hello to a
+          better hangboarding experience. Welcome to the Hangboarding
+          Application!
+        </p>
+
+        <p>
+          This application is designed for the climbers to have a hands off hang
+          boarding experience. With repeaters having a mini set within the
+          exercise itself, it is hard to ensure one is starting and stopping
+          when they should as well as potentially having to handling the phone
+          when you need to start another timer for resting between the exercise
+          sets.
+        </p>
+        <p>
+          With the Hangboarding Application, you can set the repeaters to your
+          desired time specifications, press start, and follow along handsfree.
+          Enjoy and be on the lookout for updates soon! RLL
+        </p>
+      </div>
     </div>
   );
 }
