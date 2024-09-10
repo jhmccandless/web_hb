7aedde43428fcec98725b82957599132a2098d66
Jason McCandless <jhmccandless1105@gmail.com>
2024-09-10 01:23:06 -0500

    styling with the main timer is included

--- src/components/OnOffTimerDetails.tsx
+++ src/components/OnOffTimerDetails.tsx
@@ -6,7 +6,7 @@ import IncrementTime from "./IncrementTime";
 function OnOffTimerDetails(props: TimerDetailsProps) {
   return (
     <div className="timer-wrapper-details">
-      <div className="timer-item-1-2-single">
+      <div className="timer-item-1-2-single timer-item-1-land-center">
         <p>Total Workout:</p>
         <p>{secondsToTimeString(props.totalWorkout)}</p>
       </div>
@@ -15,19 +15,19 @@ function OnOffTimerDetails(props: TimerDetailsProps) {
         currentAct={props.currentAct}
         actionTime={props.actionTime}
         timerState={props.timerState.hangTime}
-        stylingProp="timer-item-1-1 timer-item-left timer-item-1-1-land"
+        stylingProp="timer-item-1-1 timer-item-left timer-item-2-1-land"
       />
       <IncrementTime
         action={"rest"}
         currentAct={props.currentAct}
         actionTime={props.actionTime}
         timerState={props.timerState.restTime}
-        stylingProp="timer-item-1-3 timer-item-right timer-item-1-3-land"
+        stylingProp="timer-item-1-3 timer-item-right timer-item-2-3-land"
       />
       <IncrementCounter
         whichCounter={"sets"}
         counter={props.setsCounter}
-        stylingProp="timer-item-2-2 timer-item-2-2-land"
+        stylingProp="timer-item-2-2 timer-item-3-2-land"
       />
     </div>
   );
--- src/components/RepeaterTimerDetails.tsx
+++ src/components/RepeaterTimerDetails.tsx
@@ -6,7 +6,7 @@ import IncrementTime from "./IncrementTime";
 function RepeaterTimerDetails(props: TimerDetailsProps) {
   return (
     <div className="timer-wrapper-details">
-      <div className="timer-item-1-2-single">
+      <div className="timer-item-1-2-single timer-item-1-land-center">
         <p>Total Workout:</p>
         <p>{secondsToTimeString(props.totalWorkout)}</p>
       </div>
@@ -15,31 +15,31 @@ function RepeaterTimerDetails(props: TimerDetailsProps) {
         currentAct={props.currentAct}
         actionTime={props.actionTime}
         timerState={props.timerState.hangTime}
-        stylingProp="timer-item-2-1 timer-item-1-1-land"
+        stylingProp="timer-item-2-1 timer-item-2-1-land"
       />
       <IncrementTime
         action={"off"}
         currentAct={props.currentAct}
         actionTime={props.actionTime}
         timerState={props.timerState.offTime}
-        stylingProp="timer-item-2-2 timer-item-1-3-land"
+        stylingProp="timer-item-2-2 timer-item-2-3-land"
       />
       <IncrementTime
         action={"rest"}
         currentAct={props.currentAct}
         actionTime={props.actionTime}
         timerState={props.timerState.restTime}
-        stylingProp="timer-item-2-3 timer-item-1-2-land"
+        stylingProp="timer-item-2-3 timer-item-2-2-land"
       />
       <IncrementCounter
         whichCounter={"reps"}
-        stylingProp="timer-item-3-1 timer-item-left timer-item-2-2-land"
+        stylingProp="timer-item-3-1 timer-item-left timer-item-3-2-land"
         counter={props.repsCounter}
       />
       <IncrementCounter
         whichCounter={"sets"}
         counter={props.setsCounter}
-        stylingProp="timer-item-3-3 timer-item-right timer-item-3-3-land"
+        stylingProp="timer-item-3-3 timer-item-right timer-item-4-3-land"
       />
     </div>
   );
