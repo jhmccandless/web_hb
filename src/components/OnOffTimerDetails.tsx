87a7ea685e73ea4c7dc405203f33365ec940ec10
Jason McCandless <jhmccandless1105@gmail.com>
2024-09-10 01:28:28 -0500

    adjusted styling for the on-off too

--- src/components/OnOffTimerDetails.tsx
+++ src/components/OnOffTimerDetails.tsx
@@ -15,19 +15,19 @@ function OnOffTimerDetails(props: TimerDetailsProps) {
         currentAct={props.currentAct}
         actionTime={props.actionTime}
         timerState={props.timerState.hangTime}
-        stylingProp="timer-item-1-1 timer-item-left timer-item-2-1-land"
+        stylingProp="timer-item-2-1 timer-item-left timer-item-2-1-land"
       />
       <IncrementTime
         action={"rest"}
         currentAct={props.currentAct}
         actionTime={props.actionTime}
         timerState={props.timerState.restTime}
-        stylingProp="timer-item-1-3 timer-item-right timer-item-2-3-land"
+        stylingProp="timer-item-2-3 timer-item-right timer-item-2-3-land"
       />
       <IncrementCounter
         whichCounter={"sets"}
         counter={props.setsCounter}
-        stylingProp="timer-item-2-2 timer-item-3-2-land"
+        stylingProp="timer-item-3-2 timer-item-3-2-land"
       />
     </div>
   );
