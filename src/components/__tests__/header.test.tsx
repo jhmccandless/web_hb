32dbd118014cd0777d2ce7f0e45f39bfc335c0f5
Jason McCandless <jhmccandless1105@gmail.com>
2024-10-26 18:27:00 -0500

    some more tests

--- src/components/__tests__/header.test.tsx
+++ src/components/__tests__/header.test.tsx
@@ -1,15 +1,20 @@
 import { render, screen } from "@testing-library/react";
 import Header from "../Header";
 import { SetStateAction } from "react";
+import { BrowserRouter, Routes } from "react-router-dom";
 
 test("should render header", () => {
   render(
-    <Header
-      menuOpen={false}
-      toggleButton={function (value: SetStateAction<boolean>): void {
-        throw new Error("Function not implemented.");
-      }}
-    />
+    <BrowserRouter>
+      <Routes>
+        <Header
+          menuOpen={false}
+          toggleButton={function (value: SetStateAction<boolean>): void {
+            throw new Error("Function not implemented.");
+          }}
+        />
+      </Routes>
+    </BrowserRouter>
   );
   const headerElement = screen.getByTestId("header1");
 });
