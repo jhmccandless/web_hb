ce6c100fa2121f2fdc7c4ec4ee0c5ef7753280f0
Jason McCandless <jhmccandless1105@gmail.com>
2024-12-21 16:30:55 -0600

    describe added

--- src/components/IncrementTime.test.tsx
+++ src/components/IncrementTime.test.tsx
@@ -2,17 +2,19 @@ import { render, screen } from "@testing-library/react";
 import IncrementTime from "./IncrementTime";
 
 const mockData = {
-  action: 'hang',
-  currentAct: 'hang',
+  action: "hang",
+  currentAct: "hang",
   actionTime: 12,
   timerState: 12,
-  stylingProp: 'black',
-}
+  stylingProp: "black",
+};
+describe("incrementtime", () => {
+  render(<IncrementTime {...mockData} />);
 
-test(" renders successfully", () => {
-  render(<IncrementTime {...mockData}/>);
-  const element = screen.getByText(/hang/i);
-  expect(element).toBeInTheDocument();
-})
+  test("Action renders successfully", () => {
+    const element = screen.getByText(/hang/i);
+    expect(element).toBeInTheDocument();
+  });
 
-test('')
+  test("time renders successfully", () => {});
+});
